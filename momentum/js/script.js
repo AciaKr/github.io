cd689c4f523c2580118d71cc1cfb325aba6385d3
AciaKr <krasnastassia@gmail.com>
2023-02-19 13:58:53 +0300

    feat: add switch background image

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,34 +1,44 @@
+
+const date = new Date();
+const hours = date.getHours();
+const timeOfDay = getTimeOfDay(hours);
+const body = document.body;
+const time = document.querySelector('.time');
+const showedDate = document.querySelector('.date');
+const greeting = document.querySelector('.greeting');
+const nameUser = document.getElementsByTagName("input")[1];
+nameUser.placeholder = '[Enter name]';
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
+const playBtn = document.querySelector('.play');
+const audio = new Audio();
+
+
 // Show Current Time
+showTime();
+
 function showTime() {
-    const date = new Date();
-    const time = document.querySelector('.time');
     const currentTime = date.toLocaleTimeString();
     time.textContent = currentTime;
     showDate();
-    showGreeting()
+    showGreeting();
     setTimeout(showTime, 1000);
 }
 
 // Show Current Date
 function showDate() {
-    const date = new Date();
-    const showedDate = document.querySelector('.date');
     const options = {weekday: 'long', month: 'long', day: 'numeric', year: 'numeric', timeZone: 'UTC'};
     const currentDate = date.toLocaleDateString('en-US', options);
     showedDate.textContent = currentDate;
 }
-showTime();
 
 // Show Greeting
 function showGreeting() {
-    const date = new Date();
-    const hours = date.getHours();
-    const timeOfDay = getTimeOfDay(hours);
     const greetingText = `Good ${timeOfDay}`;
-    const greeting = document.querySelector('.greeting');
     greeting.textContent = greetingText;
 }
 
+// Choice time of day
 function getTimeOfDay(hours) {
     const rate = hours/6;
     if (0 <= rate && rate < 1) { return 'night'; }
@@ -40,13 +50,13 @@ function getTimeOfDay(hours) {
 // Input/Output greeting name
 window.addEventListener('beforeunload', setLocalStorage)
 window.addEventListener('load', getLocalStorage)
-const nameUser = document.getElementsByTagName("input")[1];
-nameUser.placeholder = '[Enter name]';
 
+// Save greeting name in localStorage
 function setLocalStorage() {
     localStorage.setItem('name', nameUser.value);
 }
 
+// Get greeting name from localStorage
 function getLocalStorage() {
     if(localStorage.getItem('name')) {
         nameUser.value = localStorage.getItem('name');
@@ -54,5 +64,72 @@ function getLocalStorage() {
 }
 
 // Change background Image
-const body = document.body;
-body.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/18.jpg')";
+let randomNum = getRandomNum(1, 20);
+
+// Set background Image
+function setBg() {
+    const img = new Image();
+    const bgNum = randomNum.toString().padStart(2,'0');
+    img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
+    img.onload = () => {
+      body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`;
+    };
+}
+setBg()
+
+// function create random number image included min and max
+function getRandomNum(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function getSlideNext
+function getSlideNext() {
+    if (randomNum == 20) {
+        randomNum = 1;
+    } else {
+        randomNum += 1;
+    }
+    setBg(randomNum);
+}
+
+// function getSlidePrev
+function getSlidePrev() {
+    if (randomNum == 1) {
+        randomNum = 20;
+    } else {
+        randomNum -= 1;
+    }
+    setBg(randomNum);
+}
+
+slideNext.addEventListener('click', getSlideNext)
+slidePrev.addEventListener('click', getSlidePrev)
+
+// Audio player
+let isPlay = false;
+
+function playAudio() {
+    audio.src = 'https://7oom.ru/audio/naturesounds/07%20Birds%20(7oom.ru).mp3';
+    if(!isPlay) {
+        audio.currentTime = 0;
+        audio.play();
+        isPlay = true;
+    } else {
+        audio.pause();
+        isPlay = false;
+    }
+}
+
+// Switch icon player
+function toggleBtn() {
+    if(!isPlay) {
+        playBtn.classList.remove('pause');
+    } else {
+        playBtn.classList.add('pause');
+    }
+}
+
+  playBtn.addEventListener('click', playAudio);
+  playBtn.addEventListener('click', toggleBtn);
